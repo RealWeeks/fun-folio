06446d08a88740525cf1e80a9e914388abdf279c
RealWeeks <weeksjasons@gmail.com>
2018-12-31 11:05:16 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,74 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link class="nav-btn nav-home" to="/"> <div>Home</div></router-link>
+      <router-link class="nav-btn nav-about" to="/about"><div>About</div></router-link>
+      <router-link class="nav-btn nav-work" to=""><div>Work</div></router-link>
+      <router-link class="nav-btn nav-resume" to=""><div>Resume</div></router-link>
+      <router-link class="nav-btn nav-contact" to=""><div>Contact</div></router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+      return {
+        mouseHome: false,
+      }
+    },
+}
+</script>
+
 <style>
+html, body {height: 100%}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
 }
 #nav {
-  padding: 30px;
+  /* padding: 30px; */
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.nav-btn{
+  text-decoration: none;
+  height: 80px;
+  width: 20%;
+}
+.nav-btn div{
+  margin-top: 10%;
+  color: white;
+  font-size: 3.5em;
+}
+.nav-home{
+  background-color: #173753;
+}
+
+.nav-about{
+  background-color: #6DAEDB;
+}
+
+.nav-work{
+  background-color: #2892D7;
+}
+
+.nav-resume{
+  background-color: #1B4353;
+}
+
+.nav-contact{
+  background-color: #1D70A2;
 }
 
 #nav a {
@@ -25,7 +76,8 @@
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.nav-btn:hover{
+  font-size: small;
+  align-self: flex-start;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="text-wrapper">
+      <div class="line-one">Hello,</div>
+      <div class="line-two">I'm Jason</div>
+      <br/>
+      <div class="line-three">Developer, Enginenner,</div>
+      <div class="line-four">{{selectedSkill}}.</div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
+  created(){
+    this.selectedSkill = 'human'
+  },
+  watch: {
+    selectedSkill(){
+      let self = this;
+      setTimeout(function(){
+      let array = self.randomSkill
+      self.selectedSkill = array[Math.floor(Math.random() * array.length)];
+    }, 2000);
+    }
+  },
+  methods:{
+  },
+  data() {
+    return {
+      randomSkill: ['Friend', 'Gamer', 'Patriots fan', 'Dog lover', 'environmentalist'],
+      selectedSkill: ''
+    }
+  },
 }
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Playfair+Display|Poppins');
+.home{
+  height: 100%;
+  background-color: #173753;
+  color: white;
+}
+.text-wrapper{
+  padding-top: 5%;
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+}
+.line-one{
+  margin-left: 5%;
+  font-size: 5em;
+  font-family: 'Poppins', sans-serif;
+}
+.line-two{
+  font-size: 5em;
+  margin-left: 5%;
+  font-family: 'Poppins', sans-serif;
+}
+.line-three{
+  font-size: 5em;
+  margin-left: 15%;
+  font-family: 'Playfair Display', serif;
+}
+.line-four{
+  font-size: 5em;
+  margin-left: 15%;
+  font-family: 'Playfair Display', serif;
+  font-style: italic;
+}
+</style>
